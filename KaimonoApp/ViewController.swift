d3bbc0e71b9f4dd906b37d2acc0556b56243a637
tarako0 <icerate149@gmail.com>
2023-05-26 20:29:05 +0900

    first commit

---
+++ KaimonoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KaimonoApp
+//
+//  Created by 上田明人 on 2023/05/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
